5255257428b7fb60508ae4160786a5cb55278049
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-06-17 23:17:19 -0300

    feat: authenticate middleware market

---
+++ src/middleware/authenticateMarket.ts
@@ -0,0 +1,31 @@
+import { Response, Request, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+dotenv.config()
+
+export async function AuthenticateMarket(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+    const token = req.headers.authorization.split(' ')[1]
+
+    if (!token) return res.status(401).send(unauthorizedError())
+
+    const { market_id } = jwt.verify(token, process.env.JWT_SECRET_KEY) as JWTPayload
+
+    if(!market_id) return res.status(401).send(unauthorizedError())
+
+    req.market_id = market_id
+
+    next()
+}
+
+function unauthorizedError() {
+    return {
+        name: 'UnauthorizedError',
+        message: 'unauthorized'
+    }
+}
+
+export type AuthenticatedRequest = Request & JWTPayload;
+
+type JWTPayload = {
+    market_id: number;
+};
